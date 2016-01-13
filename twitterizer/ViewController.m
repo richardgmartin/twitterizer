c832062d748c2022301df93b236d1164308de709
erd-s <chris.erdos@gmail.com>
2016-01-13 15:43:28 -0600

    we finished twitterize

--- twitterizer/ViewController.m
+++ twitterizer/ViewController.m
@@ -23,22 +23,27 @@
 }
 
 - (IBAction)onTwitterizeButtonTapped:(UIButton *)sender {
-    
-    NSString *textViewString = self.textView.text;
-    
-    NSMutableString *twitterizedString = [NSMutableString new];
-    
-    for (int i = 0 ; i <= textViewString.length; i++) {
-        if ([textViewString characterAtIndex:i != @"a" || @"e" || @"i" || @"o" || @"u"]) {
-            
-            NSString *tempFirstPart = [NSString stringWithFormat:@"%c", [textViewString characterAtIndex:i]];
-            
-            [twitterizedString appendString:tempFirstPart];
-            
-        }
-    }
-    
+	
+	NSString *textViewString = self.textView.text;
+	
+	NSMutableString *twitterizedString = [NSMutableString new];
+	NSMutableArray *arrayWithCharacters = [NSMutableArray new];
+	NSArray *vowelsArray = @[@"a", @"e", @"i", @"o", @"u"];
+	
+		for (int i = 0 ; i <= (textViewString.length - 1); i++) {
+			NSString *letters = [NSString stringWithFormat:@"%c", [textViewString characterAtIndex:i]];
+			[arrayWithCharacters addObject:letters];
+			NSLog(@"%@", arrayWithCharacters[i]);
+			
+			if (!([vowelsArray[0] isEqualToString:arrayWithCharacters[i]]
+				  || [vowelsArray[1] isEqualToString:arrayWithCharacters[i]]
+					 || [vowelsArray[2] isEqualToString:arrayWithCharacters[i]]
+						 || [vowelsArray[3] isEqualToString:arrayWithCharacters[i]]
+							 || [vowelsArray[4] isEqualToString:arrayWithCharacters[i]])) {
+				[twitterizedString appendString:arrayWithCharacters[i]];
+			}
+		
+		self.textView.text = twitterizedString;
+	}
 }
-
-
 @end
